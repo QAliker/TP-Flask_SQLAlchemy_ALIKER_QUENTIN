d7e071b6d9f51366d509b9dbbd74d0161eec036e
Mostapha <mossb.addi@gmail.com>
2024-02-07 10:08:21 +0100

    Fixed Inscription Form

--- src/critique_film/__init__.py
+++ src/critique_film/__init__.py
@@ -8,6 +8,7 @@ migrate = Migrate()
 def create_app():
     app = Flask(__name__)
     # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://username:password@domaine:port/database'
+    app.config['SECRET_KEY'] = 'mysecretkey'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:pass@db/critiques_de_films'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
--- src/critique_film/routes.py
+++ src/critique_film/routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from .forms import InscriptionForm
 
 main = Blueprint('main', __name__)
 
@@ -6,3 +7,9 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def index():
     return render_template('index.html')
+
+@main.route('/inscription', methods=['GET', 'POST'])
+def inscription():
+    # opérations pour enregistrer le formulaire
+    form = InscriptionForm()
+    return render_template('inscription.html', form=form)
